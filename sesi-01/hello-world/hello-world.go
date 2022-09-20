efc77847dd3411e20674b4ed458866bde722cc0e
Dita-L <110955010+Dita-L@users.noreply.github.com>
2022-09-20 14:49:29 +0700

    Update hello-world.go

--- sesi-01/hello-world/hello-world.go
+++ sesi-01/hello-world/hello-world.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	//Belajar Hello
 	fmt.Println("Hello World")
+	
+	//Println dan Print
+	fmt.Print("Halo", "Halo", "Bandung\n")
+	fmt.Print("Halo", "", "Halo", "Bandung\n")
+	fmt.Print("Halo", " Halo", " Bandung\n")
+	
 }
