1563fe09bb5301c49b281d44d105b334cfd01f63
Dita Lastri <110955010+Dita-L@users.noreply.github.com>
2022-10-27 19:04:22 +0700

    Update photoController.go

--- final/controllers/photoController.go
+++ final/controllers/photoController.go
@@ -15,22 +15,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// type respGetUser struct {
-// 	username string //`json:"username"`
-// 	email    string //`json:"email"`
-// }
-
-// type respGet struct {
-// 	id         uint      //`json:"photo_id"`
-// 	title      string    //`json:"title"`
-// 	caption    string    //`json:"caption"`
-// 	photo_url  string    //`json:"photo_url"`
-// 	user_id    uint      //`json:"user_id"`
-// 	created_at time.Time //`json:"created_at"`
-// 	updated_at time.Time //`json:"updated_at"`
-// 	//user       respGetUser //`json:"user"`
-// }
-
 func CreatePhoto(c *gin.Context) {
 	//connect database
 	db := database.GetDB()
@@ -105,38 +89,8 @@ func GetAllPhoto(c *gin.Context) {
 		return
 	}
 
-	// for i := 0; i < len(photo); i++ {
-	// 	dataResp := respGet{
-	// 		id:         photo[i].ID,
-	// 		title:      photo[i].Title,
-	// 		caption:    photo[i].Caption,
-	// 		photo_url:  photo[i].Photo_URL,
-	// 		user_id:    photo[i].User_ID,
-	// 		created_at: photo[i].CreatedAt,
-	// 		updated_at: photo[i].UpdatedAt,
-	// 	}
-	// 	a = append(a, dataResp)
-	// }
-
 	c.JSON(http.StatusOK, photo)
 }
-
-// func abc(photo *[]models.Photo) []respGet {
-// 	var a []respGet
-// 	for _, v := range *photo {
-// 		dataResp := respGet{
-// 			id:         v.ID,
-// 			title:      v.Title,
-// 			caption:    v.Caption,
-// 			photo_url:  v.Photo_URL,
-// 			user_id:    v.User_ID,
-// 			created_at: v.CreatedAt,
-// 			updated_at: v.UpdatedAt,
-// 		}
-// 		a = append(a, dataResp)
-// 	}
-// 	return a
-// }
 
 func Updatephoto(c *gin.Context) {
 	db := database.GetDB()
